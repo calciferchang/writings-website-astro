5d4252879644b6d5da5fd1899d1511d17720449d
kiki <142045045+calciferchang@users.noreply.github.com>
2025-12-26 13:52:06 -0500

    project setup

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { defineCollection } from "astro:content";
+// Import Zod
+import { z } from "astro/zod";
+// Define a `loader` and `schema` for each collection
+const essays = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{mdx,md}", base: "./content/essays" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { essays };
